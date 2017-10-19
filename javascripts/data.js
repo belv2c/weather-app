006dfcdb56bf0b7778ad760ac9727e0dd604c490
Brooke Lewis <belv2c@gmail.com>
2017-10-18 19:11:46 -0500

    added data

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const zipCode = () => {
+	// promise for zipCode search
+};
+
+const setKey = () => {
+	// accepts a string
+};
+
+const showResults = () => {
+	// calls dom.domstring
+};
+
+module.exports = {};
