dcafc30d78229caef174d98e23b35f67bf44eb5a
Brooke Lewis <belv2c@gmail.com>
2017-10-23 14:11:59 -0500

    current weather works

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -2,23 +2,18 @@
 
 let weatherArray = 1;
 
+
+// current weather domstring 
 const buildDomString = (weatherObject) => {
 	let domString = "";
-
-	
 	let weatherArray = weatherObject.list;
-	for (let i = 0; i < weatherArray.length; i++) {
-	/*	if (i % 3 === 0) {
-		domString += `<div class="row">`;
-	}*/
-	
-	domString += `<div class="col-sm-3">`;
-	
-	domString += 	`<h1>${weatherObject.city.name}</h1>`;
-	domString +=	`<div>${weatherArray[i].main.temp} &deg F</div>`;
-	domString += 	`<p>${weatherArray[i].weather[0].description}</p>`;
-	domString += 	`<h3>${weatherArray[i].main.pressure}</h3>`;
-	domString += 	`<h3>${weatherArray[i].wind.speed}</h3>`;
+	for (let i = 0; i < 1; i++) {
+	domString += `<div class="col-xs-6 col-xs-offset-3">`;
+	domString += 	`<h1 class="name">${weatherObject.city.name}</h1>`;
+	domString += 	`<p class="description">${weatherArray[i].weather[0].description}</p>`;
+	domString +=	`<div class="currentTemp">${Math.round(weatherArray[i].main.temp)} &deg F</div>`;
+	domString += 	`<p class="air">Air pressure: ${Math.round(weatherArray[i].main.pressure)} mb</h3>`;
+	domString += 	`<p class="wind">Wind speed: ${Math.round(weatherArray[i].wind.speed)} mph</h3>`;
 	domString +=	`</section>`;
 	domString += `</div>`;
 	domString += `</main>`;
