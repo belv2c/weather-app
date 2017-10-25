96074b03b71f3400f294b08e0a8d35c86d6d0cac
Brooke Lewis <belv2c@gmail.com>
2017-10-25 02:01:04 -0500

    fixed date format and adjusted card styling

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,10 +1,12 @@
 "use strict";
 
+let moment = require('../lib/node_modules/moment/moment.js');
 
 // current weather domstring 
 const buildDomString = (weatherObject) => {
 	let domString = "";
 	let weatherArray = weatherObject.list;
+	domString += 	`<h1 class="name">${weatherObject.city.name}</h1>`;
 	for (let i = 0; i < 1; i++) {
 		let weather = weatherArray[i].weather[0].id;
 		let dayOrNight = "";
@@ -17,11 +19,12 @@ const buildDomString = (weatherObject) => {
 		}
 		let completedWeatherIcon = `<i class="wi wi-owm-${weather}"></i>`;
 	domString += `<div class="card col-xs-6 col-xs-offset-3">`;
-	domString += 	`<h1 class="name">${weatherObject.city.name}</h1>`;
-	domString +=	`<h4>${weatherArray[i].dt_txt}</h4>`;
-	domString +=	`<p>${completedWeatherIcon}</p>`;
+	
+	domString +=	`<h4>${moment.utc(weatherArray[i].dt_txt).format('dddd[, ]MMMM DD')}</h4>`;
+	domString +=	`<div class="icon">${completedWeatherIcon}</div>`;
+	domString +=	`<div class="temp">${Math.round(weatherArray[i].main.temp)} &deg F</div>`;
 	domString += 	`<p class="description">${weatherArray[i].weather[0].description}</p>`;
-	domString +=	`<div class="currentTemp">${Math.round(weatherArray[i].main.temp)} &deg F</div>`;
+	
 	domString += 	`<p class="air">Air pressure: ${Math.round(weatherArray[i].main.pressure)} mb</h3>`;
 	domString += 	`<p class="wind">Wind speed: ${Math.round(weatherArray[i].wind.speed)} mph</h3>`;
 	domString +=	`</section>`;
@@ -35,6 +38,7 @@ const buildDomString = (weatherObject) => {
 const threeDayString = (weatherObject, i) => {
 	let domString = "";
 	let weatherArray = weatherObject.list;
+	domString += 	`<h1 class="name">${weatherObject.city.name}</h1>`;
 	for (let i = 0; i < weatherArray.length; i++) {
 		if (i % 3 === 0) {
 		domString += `<div class="row container-fluid">`;
@@ -51,8 +55,8 @@ const threeDayString = (weatherObject, i) => {
 		let completedWeatherIcon = `<i class="wi wi-owm-${weather}"></i>`;
 		if (i === 4 || i === 12 || i === 20) {	
 	domString += `<div class="card col-sm-6 col-md-4">`;
-	domString += 	`<h1 class="name">${weatherObject.city.name}</h1>`;
-	domString +=	`<h4>${weatherArray[i].dt_txt}</h4>`;
+	
+	domString +=	`<h4>${moment.utc(weatherArray[i].dt_txt).format('dddd[, ]MMMM DD')}</h4>`;
 	domString +=	`<p>${completedWeatherIcon}</p>`;
 	domString += 	`<p class="description">${weatherArray[i].weather[0].description}</p>`;
 	domString +=	`<div class="currentTemp">${Math.round(weatherArray[i].main.temp)} &deg F</div>`;
@@ -71,6 +75,7 @@ const threeDayString = (weatherObject, i) => {
 const fiveDayString = (weatherObject, i) => {
 	let domString = "";
 	let weatherArray = weatherObject.list;
+	domString += 	`<h1 class="name">${weatherObject.city.name}</h1>`;
 	for (let i = 0; i < weatherArray.length; i++) {
 		if (i % 3 === 0) {
 		domString += `<div class="row container-fluid">`;
@@ -87,8 +92,8 @@ const fiveDayString = (weatherObject, i) => {
 		let completedWeatherIcon = `<i class="wi wi-owm-${weather}"></i>`;
 		if (i === 4 || i === 12 || i === 20 || i === 28 || i === 36) {	
 	domString += `<div class="card col-sm-6 col-md-4">`;
-	domString += 	`<h1 class="name">${weatherObject.city.name}</h1>`;
-	domString +=	`<h4>${weatherArray[i].dt_txt}</h4>`;
+	
+	domString +=	`<h4>${moment.utc(weatherArray[i].dt_txt).format('dddd[, ]MMMM DD')}</h4>`;
 	domString +=	`<p>${completedWeatherIcon}</p>`;
 	domString += 	`<p class="description">${weatherArray[i].weather[0].description}</p>`;
 	domString +=	`<div class="currentTemp">${Math.round(weatherArray[i].main.temp)} &deg F</div>`;
